97cc0cb6f17a3bd1902c424814862e7a2d162183
RAC22 <robrac83@gmail.com>
2023-12-19 05:21:32 -0800

    add last imgs

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-for (let i = 1; i < 182; i++) {
+for (let i = 1; i < 209; i++) {
 	console.log(
-		`<img src="https://rac22.github.io/postOne/assets/imgs/${i}.HEIC">`
+		`<img src="https://rac22.github.io/postOne/assets/imgs/${i}.jpg"/><br/>`
 	);
 }
